5cbaac297e57cdc4988e99767124e301c92eab7a
Juan Alfonso <jalfonso.edu@gmail.com>
2026-01-03 00:10:50 -0600

    Se crea estructura para usar Fluent Api Se configura el AppDbContext Min 51

---
+++ MyStore/Context/AppDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace MyStore.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Category>(e =>
+        {
+            e.HasKey("CategoryId");
+            e.Property(c => c.Name).IsRequired();
+            e.Property("CategoryId").ValueGeneratedOnAdd();
+            e.HasData(
+                new Category { CategoryId = 1, Name = "Technology" },
+                new Category { CategoryId = 2, Name = "Bedroom" },
+                new Category { CategoryId = 3, Name = "Clothing" }
+            );
+        });
+        modelBuilder.Entity<User>(e =>
+        {
+            e.HasKey("UserId");
+            e.Property("UserId").ValueGeneratedOnAdd();
+
+        });
+        modelBuilder.Entity<Order>(e =>
+        {
+            e.HasKey("OrderId");
+            e.Property("OrderId").ValueGeneratedOnAdd();
+            e.Property("TotalAmount").HasColumnType("decimal(10/2)");
+            e.HasOne(e => e.User).WithMany(p => p.Orders).HasForeignKey(p => p.UserId).
+            OnDelete(DeleteBehavior.Restrict);
+        });
+        modelBuilder.Entity<OrderItem>(e =>
+        {
+            e.HasKey("OrderItemId");
+            e.Property("OrderItemId").ValueGeneratedOnAdd();
+            e.HasOne(e => e.Order).WithMany(p => p.OrderItems).HasForeignKey(p => p.OrderId).
+            OnDelete(DeleteBehavior.Restrict);
+            e.HasOne(e => e.Product).WithMany().HasForeignKey(p => p.ProductId).
+            OnDelete(DeleteBehavior.Restrict);
+        });
+        modelBuilder.Entity<Product>(e =>
+        {
+            e.HasKey("ProductId");
+            e.Property(c => c.Name).IsRequired();
+            e.Property("ProductId").ValueGeneratedOnAdd();
+            e.Property("Price").HasColumnType("decimal(10/2)");
+            e.HasOne<Category>(c => c.Category).WithMany(p => p.Products).HasForeignKey(p => p.CategoryId).
+            OnDelete(DeleteBehavior.Restrict);
+        });
+    }
+}
--- MyStore/Entities/Category.cs
+++ MyStore/Entities/Category.cs
@@ -5,4 +5,5 @@ public class Category
     public int CategoryId { get; set; }
     [Required]
     public string Name { get; set; }
+    public ICollection<Product>? Products { get; set; }
 }
