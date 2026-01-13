1a9fc34625f73441fc1ee9686b60845ade4d94b2
Juan Alfonso <jalfonso.edu@gmail.com>
2026-01-12 22:41:44 -0600

    Se crea GenericRepository para control centralizado de las entidades y se da de alta el servicio en Program.cs

--- MyStore/Program.cs
+++ MyStore/Program.cs
@@ -4,8 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Acceso a la BD
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlString")));
+builder.Services.AddScoped(typeof(MyStore.Repositories.GenericRepository<>));//Espera la entidad
+builder.Services.AddScoped<MyStore.Services.CategoryService>();
+
 
 var app = builder.Build();
 
---
+++ MyStore/Repositories/GenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyStore.Context;
+
+namespace MyStore.Repositories;
+
+public class GenericRepository<TEntity>(AppDbContext _dbContext) where TEntity : class
+{
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dbContext.Set<TEntity>().ToListAsync();
+    }
+}
